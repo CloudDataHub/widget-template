aeac5bd9cfada0e39373301a15522c456dff1028
NaumZakletskyi <naum.zakletskyi@sysgears.com>
2024-08-20 09:53:35 +0300

    Added ws provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import {WidgetPropsProvider} from "./modules/common/providers/WidgetProps";
 import {ApiProvider} from "./modules/common/providers/Api";
 import {ExampleComponent} from "./modules/common/components/ExampleComponent";
+import {WsProvider} from "./modules/common/providers/Ws";
 
 function App() {
   return (
     <WidgetPropsProvider>
       <ApiProvider>
-        <ExampleComponent/>
+        <WsProvider>
+          <ExampleComponent/>
+        </WsProvider>
       </ApiProvider>
     </WidgetPropsProvider>
   );
--- src/modules/common/components/ExampleComponent/index.tsx
+++ src/modules/common/components/ExampleComponent/index.tsx
@@ -1,41 +1,24 @@
-import {useContext, useEffect, useState} from "react";
-import {ApiContext} from "../../providers/Api";
-import {Box, Typography} from "@mui/material";
+import {useEffect} from "react";
+import {Box} from "@mui/material";
+import {useSocket} from "../../providers/Ws";
 
 export const ExampleComponent = ()=>{
-  const {api} = useContext(ApiContext)
-  const [isLoading, setIsLoading] = useState(false)
-  const [documents, setDocuments] = useState(null)
-  
-  
-  const handleLoadingStart = ()=>{
-    setIsLoading(true)
-  }
-  const handleLoadingFinish= ()=>{
-    setIsLoading(false)
-  }
+  const ws = useSocket()
   
   useEffect(()=>{
-    const getDocuments = async () =>{
-      if(api){
-        const result = await api.documentApi.get(
-          {pageNumber: 1, limit: 10},
-          {onStart: handleLoadingStart, onFinish: handleLoadingFinish}
-        )
-        setDocuments(result)
-      }
-    }
-    
-    getDocuments()
-
-  }, [api])
+    const callback = console.log
+    ws.setConfigChangedCallback(callback)
+    return ()=> ws.removeConfigChangedCallback(callback)
+  },[])
   
   return(
-    <Box>
-      <Typography>
-        {isLoading && "isLoading"}
-        {documents && JSON.stringify(documents)}
-      </Typography>
+    <Box
+      sx={{
+        width: '100px',
+        height: '100px',
+        backgroundColor: 'red'
+      }}
+    >
     </Box>
   )
 }
